7c86facd3a67e8f8146f99138af1a8417c0938c6
schoolharuki <sonefrree58@gmail.com>
2022-05-02 16:57:35 +0900

    https::/teraoka/ikemen.com

--- script3.js
+++ script3.js
@@ -1,26 +1,46 @@
 
 
 window.onload = function () {
-    document.body.animate( {
-        background: ["black", "rgb(226, 226, 226)"]
-    }, 
-    {
-        fill: "both",
-        duration: 6000
-    }
-);
-    var scroll = document.querySelectorAll('.item');
-    var Animation = function() {
-        for(var i = 0; i < scroll.length; i++) {
-            var triggerMargin = 90;
-            if(window.innerHeight > scroll[i].getBoundingClientRect().top + triggerMargin) {
-                scroll[i].classList.add('show');
+    const Classname = 'show';
+    const timeout = 3000;
+    const $target = $('.opening-curtain');
+
+    
+    // $target.addClass(Classname);
+    setTimeout(() => {
+        $target.removeClass(Classname);
+    }, timeout);
+
+    var scroll3 = document.querySelectorAll('.home-explain');
+    var backhome = document.querySelectorAll('.back-ground-home');
+    var AnimationExplain = function() {
+        for(var i = 0; i < scroll3.length; i++) {
+            var triggerMargin = 300;
+            if(window.innerHeight > scroll3[i].getBoundingClientRect().top + triggerMargin) {
+                scroll3[i].classList.add('show');
+                backhome[i].classList.add('show');
             } else {
-                scroll[i].removeClass('show')
+                scroll3[i].classList.remove('show');
+                backhome[i].classList.remove('show')
             }
         }
     }
-    window.addEventListener('scroll', Animation);
+    window.addEventListener('scroll', AnimationExplain);
+
+
+
+    var scroll2 = document.querySelectorAll('.home-item');
+    var AnimationHome = function() {
+        for(var i = 0; i < scroll2.length; i++) {
+            var triggerMargin = 300;
+            if(window.innerHeight > scroll2[i].getBoundingClientRect().top + triggerMargin) {
+                scroll2[i].classList.add('show');
+            } else {
+                scroll2[i].classList.remove('show');
+            }
+        }
+    }
+    window.addEventListener('scroll', AnimationHome);
 
     var nav = document.getElementById('nav-wrapper');
     var hamburger = document.getElementById('js-hamburger');
@@ -32,7 +52,36 @@ window.onload = function () {
     blackBg.addEventListener('click', function () {
         nav.classList.remove('open');
     });
+
+    document.getElementById('sub-body').animate( {
+        background: ["black", "rgb(255, 255, 255)"]
+    }, 
+    {
+        fill: "both",
+        duration: 3000
+    }
+
+);
+    var scroll = document.querySelectorAll('.item');
+    var scroll2 = document.querySelectorAll('.home-item');
+
+    var Animation = function() {
+        for(var i = 0; i < scroll.length; i++) {
+            var triggerMargin = 90;
+            if(window.innerHeight > scroll[i].getBoundingClientRect().top + triggerMargin) {
+                scroll[i].classList.add('show');
+            } else {
+                scroll[i].classList.remove('show');
+            }
+        }
+    }
+    window.addEventListener('scroll', Animation);
+
 };
+
+
+
+
 
 
 $(document).ready(function(){
@@ -41,18 +90,18 @@ $(document).ready(function(){
     })
 })
 
-$(document).ready(function(){
-    $('.mem').on('click', function(){
-        $(this).next().slideToggle('mem-menu')
-    })
-})
+// $(document).ready(function(){
+//     $('.mem').on('click', function(){
+//         $(this).next().slideToggle('mem-menu')
+//     })
+// })
 
 $(document).ready(function(){
     $('a[href^="#"]').click(function(){
         var speed = 500;
         var href = $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
+        var position = target.offset().top-150;
         $("html, body").animate({scrollTop:position}, speed, "swing");
     });
 });
